5b7d1717db66d07b0660f69842dad3a73a8a3420
Chian Yein Ang <chianyein@gmail.com>
2023-07-24 14:09:00 +0800

    chore(footer): use full name

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="text-xs text-center w-full mt-5 py-3">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-            Coded by <a href="https://github.com/acyein" target="_blank">Yein</a>.
+            Coded by <a href="https://github.com/acyein" target="_blank">Ang Chian Yein</a>.
         </footer>
     );
 }
